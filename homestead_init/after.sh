ab477f177ce827262b7a30f493cb8e7e73ba91b0
ElNapoli <baldomero.napoil@gmail.com>
2016-08-28 17:15:44 -0300

    add script config laravel proyect

--- homestead_init/after.sh
+++ homestead_init/after.sh
@@ -3,3 +3,12 @@
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
+
+cd ~/projects/template
+composer install
+cp .env.example .env
+php artisan key:generate
+
+sudo apt-get install ruby-full -y
+sudo gem install compass
+
